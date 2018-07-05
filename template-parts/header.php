1af784438664618879f57b68f3b03cef8d6bede6
Alexandru Ioan Glovac <alexandruglovac@icloud.com>
2018-07-05 09:43:33 +0100

    header menu

--- template-parts/header.php
+++ template-parts/header.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+require("/functions/functions.php");
+?>
 <html lang="en" dir="ltr">
   <head>
     <link rel="stylesheet" href="/assets-minified/ext/bootstrap/css/bootstrap.min.css"/>
@@ -6,6 +9,27 @@
     <script src="/assets-minified/ext/bootstrap/js/bootstrap.min.js"/></script>
     <link rel="stylesheet" type="text/css" href="/assets-minified/css/styles.min.css">
     <meta charset="utf-8">
-    <title>Viscometer Data collection unit</title>
+    <title>Viscometer Data Collection Unit</title>
   </head>
   <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Granville SVM3000</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
