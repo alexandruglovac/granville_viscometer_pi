619adc50a859b4d814c6672ab7ffdb0993fd09aa
Alexandru Ioan Glovac <alexandruglovac@icloud.com>
2018-07-05 09:28:30 +0100

    prerequisites and minifiers installed

---
+++ commands/minify.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yui-compressor /var/www/granville_viscometer_pi/assets/css/styles.css -o /var/www/granville_viscometer_pi/assets-minified/css/styles.min.css
+yui-compressor /var/www/granville_viscometer_pi/assets/js/main.js -o /var/www/granville_viscometer_pi/assets-minified/css/main.min.js
