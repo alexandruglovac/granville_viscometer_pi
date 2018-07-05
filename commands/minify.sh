22ed5508bca0f07d0e50443c5e7f5d8d98a91203
Alexandru Ioan Glovac <alexandruglovac@icloud.com>
2018-07-05 13:22:44 +0100

    changed structure

--- commands/minify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-yui-compressor /var/www/granville_viscometer_pi/assets/css/styles.css -o /var/www/granville_viscometer_pi/assets-minified/css/styles.min.css
-yui-compressor /var/www/granville_viscometer_pi/assets/js/main.js -o /var/www/granville_viscometer_pi/assets-minified/css/main.min.js
