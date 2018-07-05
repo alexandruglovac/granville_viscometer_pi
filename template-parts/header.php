5098df757ab559d5aa6be1ca9dfdcc38140d3b12
Alexandru Ioan Glovac <alexandruglovac@icloud.com>
2018-07-05 09:37:46 +0100

    headers

---
+++ template-parts/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="/assets-minified/ext/bootstrap/css/bootstrap.min.css"/>
+    <script src="/assets-minified/ext/jquery/jquery.min.js"></script>
+    <script src="/assets-minified/ext/bootstrap/js/bootstrap.min.js"/></script>
+    <link rel="stylesheet" type="text/css" href="/assets-minified/css/styles.min.css">
+    <meta charset="utf-8">
+    <title>Viscometer Data collection unit</title>
+  </head>
+  <body>
