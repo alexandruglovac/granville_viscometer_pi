31742e472b5cd0ae427839566709cfbb6f384fd0
Alexandru Ioan Glovac <alexandruglovac@icloud.com>
2018-07-05 13:34:21 +0100

    updates

---
+++ viscometer_read_write/viscomether_usb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# Read & print/write data
+import serial
+ser = serial.Serial('/dev/ttyUSB0',19200, 8, 'N', 1, timeout = 5)
+# Open ve_direct.csv
+file_data = open('ve_data.csv', 'w')
+print "Reading data and writing to ve_data.csv"
+# Listen for the input, exit if nothing received in timeout period
+output = " "
+while True:
+  while output != "":
+    output = ser.readline()
+    file_data.write(output)
